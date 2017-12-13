7824c9fc3c1cc3c7b8cc7b1e86a24281a3a03537
DragonLi <DragonLi_52171@163.com>
2017-12-13 14:56:19 +0800

    base part .
    base part .

---
+++ base.go
@@ -0,0 +1,24 @@
+package main
+
+import LFL "fmt" // eg : 使用 . 的话 ,可以直接使用Printf
+
+//import {  // 未调用则,报编译错
+//	"os"
+//	"time"
+//}
+
+func main() {
+	LFL.Printf("base.go print test\n")
+
+	//	const round = 361  const ,like Swift let
+	//	var name = "dragonLi"  equal Swift (Global)
+
+	//	type LFLNumber int   类型申明
+
+	//	type goLang interface {
+	// API
+	//	}
+
+}
+
+// 可见性规则:首字母小写,就是 private, 大写为 public
